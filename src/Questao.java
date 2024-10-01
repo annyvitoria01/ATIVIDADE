754ddee774d8409658e4588678d87ff9dae93778
Anny <Annyvitoriasantos0103@gmail.com>
2024-10-01 20:39:20 -0300

    versao final

---
+++ src/Questao.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Questao {
+    String pergunta = "";
+    String opcaoA = "";
+    String opcaoB = "";
+    String opcaoC = "";
+    String opcaoD = "";
+    String opcaoE = "";
+    String correta = "";
+
+    public boolean isCorreta(String resposta){
+        if(resposta.equalsIgnoreCase(this.correta)){
+            System.out.println("Parabéns resposta Correta! - Letra: " + this.correta);
+            System.out.println("");
+            return true;
+        } else {
+            System.out.println("Resposta Errada!");
+            System.out.println("A opção correta é a letra: " + this.correta);
+            return false;
+        }
+    }
+
+    public String leiaResposta() {
+        Scanner ler = new Scanner(System.in);
+        String resp;
+        do {
+            System.out.println("Digite a resposta: ");
+            resp = ler.next();
+        } while (!respostaValida(resp));
+        return resp;
+    }
+
+    private boolean respostaValida(String resp){
+        if(resp.equalsIgnoreCase("A") || resp.equalsIgnoreCase("B") || resp.equalsIgnoreCase("C") ||
+                resp.equalsIgnoreCase("D") || resp.equalsIgnoreCase("E")){
+            return true;
+        }
+        System.out.println("Resposta inválida! Digite opção A, B, C, D ou E. ");
+        System.out.println("");
+        return false;
+    }{
+}
+}
